3cd0c3697e649d997d2db20d2f7a8c86b982f08b
aldous <albert.pavlik@icloud.com>
2025-03-31 16:40:32 +0200

    two tests

--- NumberEncoder/src/test/java/zest/NumberEncoderTest.java
+++ NumberEncoder/src/test/java/zest/NumberEncoderTest.java
@@ -1,6 +1,38 @@
 package zest;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class NumberEncoderTest {
 
- 
+    @Test
+    void testValidEncoding() {
+        char[] mapping = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        assertEquals("ABCDEF", NumberEncoder.encodeNumber("012345", mapping));
+        assertEquals("JIHGFE", NumberEncoder.encodeNumber("987654", mapping));
+    }
+
+    @Test
+    void testEmptyInput() {
+        char[] mapping = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        assertEquals("", NumberEncoder.encodeNumber("", mapping));
+    }
+
+    @Test
+    void testInvalidCharacter() {
+        char[] mapping = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        assertThrows(IllegalArgumentException.class, () -> NumberEncoder.encodeNumber("12a3", mapping));
+    }
+
+    @Test
+    void testNegativeNumber() {
+        char[] mapping = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        assertThrows(IllegalArgumentException.class, () -> NumberEncoder.encodeNumber("-12", mapping));
+    }
+
+    @Test
+    void testInsufficientMapping() {
+        char[] mapping = {'A', 'B', 'C', 'D', 'E'}; // Less than 10 elements
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> NumberEncoder.encodeNumber("012", mapping));
+    }
 }
--- PalindromeNumber/src/test/java/zest/PalindromeNumberTest.java
+++ PalindromeNumber/src/test/java/zest/PalindromeNumberTest.java
@@ -1,5 +1,30 @@
 package zest;
 
-class PalindromeNumberTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class PalindromeNumberTest {
+
+    @Test
+    void testPositivePalindrome() {
+        assertTrue(PalindromeNumber.isPalindrome(121));
+    }
+
+    @Test
+    void testNegativeNumber() {
+        assertFalse(PalindromeNumber.isPalindrome(-121));
+    }
+
+    @Test
+    void testSingleDigitNumber() {
+        assertTrue(PalindromeNumber.isPalindrome(7));
+    }
+
+
+    @Test
+    void testIllegalCharacter() {
+        assertThrows(NumberFormatException.class, () -> {
+            Integer.parseInt("12a21");
+        });
+    }
 }
